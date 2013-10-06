d7825fe654365f3e23a825f0f18dc85c3eb95944
smitti9 <smitti9@gmail.com>
2013-10-06 18:11:46 -0400

    Variable problems
    For whatever reason trip_length never returns what I set it to.

---
+++ rest.c
@@ -0,0 +1,17 @@
+/*Eric Smithson
+10/5/2013
+Calculates rest stops*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) {
+    int trip_length;
+    printf("How long is your road trip, in miles?\n");
+    //For whatever reason trip_length never returns what I set it to.
+    scanf("%d", &trip_length);
+    printf("%d", &trip_length);
+    /*printf("What is the distance between stops for gas, in miles?\n");
+    printf("What is the distance between stops for food, in miles?\n");
+    return 0; */
+}
